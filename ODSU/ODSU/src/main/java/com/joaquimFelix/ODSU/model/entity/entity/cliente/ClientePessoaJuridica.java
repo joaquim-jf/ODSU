e62223ec714e6c23a9a138df2814e116406bf2fc
Joaquim Felix <65045562+joaquim-jf@users.noreply.github.com>
2023-11-16 11:01:04 -0300

    Add files via upload

---
+++ ODSU/ODSU/src/main/java/com/joaquimFelix/ODSU/model/entity/entity/cliente/ClientePessoaJuridica.java
@@ -0,0 +1,14 @@
+package com.joaquimFelix.ODSU.model.entity.cliente;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@DiscriminatorValue(value = "ClientePessoaJuridica")
+public class ClientePessoaJuridica extends Cliente {
+    @NotNull
+    @Size(max = 14,min = 14,message = "colocar o cnpj todo")
+    private String cnpj;
+}
