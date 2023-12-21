839af1a580bdc12835c7a6429ea84313d90b8171
Joaquim Felix <65045562+joaquim-jf@users.noreply.github.com>
2023-12-20 23:45:17 -0300

    Update

--- ODSU/ODSU/src/main/java/com/joaquimFelix/ODSU/OdsuApplication.java
+++ ODSU/ODSU/src/main/java/com/joaquimFelix/ODSU/OdsuApplication.java
@@ -1,9 +1,12 @@
 package com.joaquimFelix.ODSU;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "OdsuAPI", version = "1", description = "Sistema de Ordem de Serviço Automotivo,"))
 public class OdsuApplication {
 
 	public static void main(String[] args) {
